c4f226640c2190ef093dd9c4c2b5d655e3a0c477
Alexander <bladyalexander333@gmail.com>
2021-08-18 10:05:47 -0500

    add sort algorithms - bubble sort

---
+++ sort_algorithms/bubble_sort.py
@@ -0,0 +1,21 @@
+import random
+
+def bubble_sort(my_list):
+    n = len(my_list)
+
+    for i in range(n):
+        for j in range(0,n -i -1):
+
+            if my_list[j] > my_list[j+1]:
+                my_list[j], my_list[j+1] = my_list[j+1], my_list[j]
+
+    return my_list
+
+
+if __name__ == '__main__':
+    size_list = int(input('What size is the list ? '))
+
+    my_list = [random.randint(0,100) for i in range(size_list)]
+
+    list_order = bubble_sort(my_list)
+    print(list_order)
